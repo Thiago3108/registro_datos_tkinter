4a337de082672fff5c51cae690c561cbd1235e16
Thiago3108 <santiago2234508@correo.uis.edu.co>
2023-05-06 16:26:00 -0400

    Se actualizo el codigo

--- programa.py
+++ programa.py
@@ -4,7 +4,7 @@
 
 # se importa la libreria tkinter con todas sus funciones
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 
 #-------------------------
 # funciones de la app
@@ -51,7 +51,6 @@ def abrir_toplevel_notas():
     # caja de texto para valor de la nota 2
     entry_notas = Entry(frame_entrada, textvariable=nota2)
     entry_notas.config(bg="white", fg="black", font=("Times New Roman", 18), width=6)
-    entry_notas.focus_set()
     entry_notas.place(x=120,y=60)
 
         # etiqueta para valor de la nota 3
@@ -62,8 +61,15 @@ def abrir_toplevel_notas():
     # caja de texto para valor de la nota 3
     entry_notas = Entry(frame_entrada, textvariable=nota3)
     entry_notas.config(bg="white", fg="black", font=("Times New Roman", 18), width=6)
-    entry_notas.focus_set()
     entry_notas.place(x=120,y=100)
+
+    # lista para kelvin y fahrenheit
+    cmb_kf = ttk.Combobox(frame_entrada, textvariable=prom_selected, values=prom, font=("Helvetica", 12))
+    cmb_kf.place(x=250, y=30, width= 100, height=30)
+
+    # boton para aceptar 
+    bt_aceptar = Button(frame_entrada,text="Aceptar", command=aceptar)
+    bt_aceptar.place(x=250, y=70, width=100, height=30)
 
 # aceptar
 def aceptar():
@@ -72,13 +78,13 @@ def aceptar():
     toplevel_notas.destroy()
 
 # convertir
-def convertir():
-    messagebox.showinfo("Temperatura 1.0", "Conversión realizada")
+def promediar():
+    messagebox.showinfo("Promedio de notas", "Promedio realizado")
     # cent = int(c.get())
-    if kf.get()=="kelvin":
+    if prom.get()=="kelvin":
         k = cent + 273.15
         t_resultados.insert(INSERT, f"\n{int(notas.get())} °C equivalen a {k} °K")
-    elif kf.get() == "fahrenheit":
+    elif prom.get() == "fahrenheit":
         f = cent*9/5 + 32
         t_resultados.insert(INSERT, f"\n{int(notas.get())} °C equivalen a {f} °F")
     else:
@@ -86,13 +92,13 @@ def convertir():
     
 # borrar
 def borrar():
-    messagebox.showinfo("Temperatura 1.0", "Los datos serán borrados")
+    messagebox.showinfo("Registro de estudiante", "Los datos serán borrados")
     notas.set("")
     t_resultados.delete("1.0","end")
 
 # salir
 def salir():
-    messagebox.showinfo("Temperatura 1.0", "La app se va a cerrar")
+    messagebox.showinfo("Registro de estudiante", "La app se va a cerrar")
     ventana_principal.destroy()
 
 #-----------------------------
@@ -117,8 +123,11 @@ ventana_principal.config(bg="khaki")
 #--------------------------------
 # variables globales
 #--------------------------------
-notas,nota2,nota3 = StringVar(), StringVar(), StringVar()
-kf = StringVar()
+notas,nota2,nota3,name,cod = StringVar(), StringVar(), StringVar(), StringVar(), StringVar()
+prom = StringVar()
+prom = ["Fundamentos de programacion", "Calculo I", "Alegebra lineal", "Quimica Basica", "Taller de lenguaje"]
+prom_selected = StringVar()
+
 
 
 #--------------------------------
@@ -131,13 +140,42 @@ frame_entrada.place(x=10, y=10)
 
 # titulo de la app
 titulo = Label(frame_entrada, text="Registro de estudiante")
-titulo.config(bg = "white",fg="dark khaki", font=("Helvetica", 20))
+titulo.config(bg = "white",fg="dark khaki", font=("MuseJazz Text", 20))
 titulo.place(x=10,y=10)
 
-# boton para abrir Toplevel para ingresar notas
-bt_centigrados = Button(frame_entrada, text="Ingresar notas", command=abrir_toplevel_notas)
-bt_centigrados.place(x=10, y=60)
+# etiqueta para valor del nombre
+lb_c = Label(frame_entrada, text = "Nombre")
+lb_c.config(bg="white", fg="black", font=("MuseJazz Text", 12))
+lb_c.place(x=10, y=50)
 
+# caja de texto para el nombre
+entry_c = Entry(frame_entrada, textvariable=name)
+entry_c.config(bg="white", fg="blue", font=("Times New Roman", 10), width=30)
+entry_c.focus_set()
+entry_c.place(x=10,y=80)
+
+# etiqueta para valor del codigo de estudiante
+lb_c = Label(frame_entrada, text = "Codigo de estudiante")
+lb_c.config(bg="white", fg="black", font=("MuseJazz Text", 12))
+lb_c.place(x=10, y=105)
+
+# caja de texto para el codigo de estudiante 
+entry_c = Entry(frame_entrada, textvariable=cod)
+entry_c.config(bg="white", fg="blue", font=("Times New Roman", 10), width=30)
+entry_c.place(x=10,y=135)
+
+# boton para abrir Toplevel para ingresar notas
+# logo de la app
+#logo = PhotoImage(file="img/temperatura.png")
+#lb_logo = Label(frame_entrada, image=logo, bg="white")
+#lb_logo.place(x=70,y=40)
+notas = PhotoImage(file="notas.png")
+bt_centigrados = Button(frame_entrada,image=notas ,text="Ingresar notas", command=abrir_toplevel_notas)
+bt_centigrados.place(x=290, y=60, width=100)
+
+# boton para abrir Toplevel para ingresar datos medicos
+bt_medico = Button(frame_entrada, text="Ingresar datos", command=abrir_toplevel_notas)
+bt_medico.place(x=430, y=60, width=100)
 
 #--------------------------------
 # frame operaciones
@@ -147,7 +185,7 @@ frame_operaciones.config(bg="white", width=580, height=100)
 frame_operaciones.place(x=10, y=200)
 
 # boton para convertir
-bt_convertir = Button(frame_operaciones,text="Convertir", command=convertir)
+bt_convertir = Button(frame_operaciones,text="Promediar", command=promediar)
 bt_convertir.place(x=45, y=35, width=100, height=30)
 
 # boton para borrar
